672e09f5665f8f97b58f8e221d76b3bc24c197f7
Tucker VandenBos <tuckervbos@gmail.com>
2025-05-24 17:24:13 -0500

    valid-sudoku-

---
+++ content/week1/day2/valid-sudoku.py
@@ -0,0 +1,87 @@
+# valid-sudoku
+# Determine if a 9 x 9 Sudoku board is valid. Only the filled cells 
+# need to be validated according to the following rules:
+
+# Each row must contain the digits 1-9 without repetition.
+# Each column must contain the digits 1-9 without repetition.
+# Each of the nine 3 x 3 sub-boxes of the grid must contain 
+# the digits 1-9 without repetition.
+
+# Note:
+# A Sudoku board (partially filled) could be valid but is not necessarily solvable.
+# Only the filled cells need to be validated according to the mentioned rules.
+
+# example 1
+# Input: board = 
+# [["5","3",".",".","7",".",".",".","."]
+# ,["6",".",".","1","9","5",".",".","."]
+# ,[".","9","8",".",".",".",".","6","."]
+# ,["8",".",".",".","6",".",".",".","3"]
+# ,["4",".",".","8",".","3",".",".","1"]
+# ,["7",".",".",".","2",".",".",".","6"]
+# ,[".","6",".",".",".",".","2","8","."]
+# ,[".",".",".","4","1","9",".",".","5"]
+# ,[".",".",".",".","8",".",".","7","9"]]
+# Output: true
+
+# Example 2:
+# Input: board = 
+# [["8","3",".",".","7",".",".",".","."]
+# ,["6",".",".","1","9","5",".",".","."]
+# ,[".","9","8",".",".",".",".","6","."]
+# ,["8",".",".",".","6",".",".",".","3"]
+# ["4",".",".","8",".","3",".",".","1"]
+# ,["7",".",".",".","2",".",".",".","6"]
+# ,[".","6",".",".",".",".","2","8","."]
+# ,[".",".",".","4","1","9",".",".","5"]
+# ,[".",".",".",".","8",".",".","7","9"]]
+# Output: false
+# Explanation: Same as Example 1, except with the 5 in the top left corner being modified to 8. Since there are two 8's in the top left 3x3 sub-box, it is invalid.
+ 
+# Constraints:
+# board.length == 9
+# board[i].length == 9
+# board[i][j] is a digit 1-9 or '.'
+
+from collections import defaultdict
+
+class Solution:
+    def isValidSudoku(self, board = list[list[str]]) -> bool:
+        row = defaultdict(set)
+        col = defaultdict(set)
+        box = defaultdict(set)
+        for r in range(len(board)):
+            for c in range(len(board[0])):
+                curr_val = board[r][c]
+                if curr_val != '.':
+                    box_coord = f'{r//3},{c//3}'
+                    if curr_val in row[r] or curr_val in col[c] or curr_val in box[box_coord]:
+                        return False
+                    row[r].add(curr_val)
+                    col[c].add(curr_val)
+                    box[box_coord].add(curr_val)
+        return True
+
+board = [["5","3",".",".","7",".",".",".","."]
+,["6",".",".","1","9","5",".",".","."]
+,[".","9","8",".",".",".",".","6","."]
+,["8",".",".",".","6",".",".",".","3"]
+,["4",".",".","8",".","3",".",".","1"]
+,["7",".",".",".","2",".",".",".","6"]
+,[".","6",".",".",".",".","2","8","."]
+,[".",".",".","4","1","9",".",".","5"]
+,[".",".",".",".","8",".",".","7","9"]]
+
+print(Solution().isValidSudoku(board)) # True
+
+board2 = [["8","3",".",".","7",".",".",".","."]
+,["6",".",".","1","9","5",".",".","."]
+,[".","9","8",".",".",".",".","6","."]
+,["8",".",".",".","6",".",".",".","3"]
+,["4",".",".","8",".","3",".",".","1"]
+,["7",".",".",".","2",".",".",".","6"]
+,[".","6",".",".",".",".","2","8","."]
+,[".",".",".","4","1","9",".",".","5"]
+,[".",".",".",".","8",".",".","7","9"]]
+
+print(Solution().isValidSudoku(board2)) # False
